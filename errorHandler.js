e48ff814063a5c7b4dae73d82578b165faf1cd2b
Mrigank Raj Dubey <97963422+mrigankraj@users.noreply.github.com>
2024-10-09 17:00:59 +0530

    Update errorHandler.js

--- errorHandler.js
+++ errorHandler.js
@@ -2,10 +2,30 @@ const errorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack
+    });
+  } else if (process.env.NODE_ENV === 'production') {
+    // Operational, trusted error: send message to client
+    if (err.isOperational) {
+      res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+      });
+    } 
+    // Programming or other unknown error: don't leak error details
+    else {
+      console.error('ERROR 💥', err);
+      res.status(500).json({
+        status: 'error',
+        message: 'Something went very wrong!'
+      });
+    }
+  }
 };
 
 module.exports = errorHandler;
