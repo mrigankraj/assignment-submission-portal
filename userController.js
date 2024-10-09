32049019e8d17211c4e280cb1273d1fd4ef3afca
Mrigank Raj Dubey <97963422+mrigankraj@users.noreply.github.com>
2024-10-09 15:48:43 +0530

    userController.js
    User-Controller

---
+++ userController.js
@@ -0,0 +1,109 @@
+const User = require('../models/user');
+const Assignment = require('../models/assignment');
+const jwt = require('jsonwebtoken');
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+exports.register = async (req, res) => {
+  try {
+    const newUser = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      role: 'user',
+    });
+
+    const token = signToken(newUser._id);
+
+    res.status(201).json({
+      status: 'success',
+      token,
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Please provide email and password',
+      });
+    }
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return res.status(401).json({
+        status: 'fail',
+        message: 'Incorrect email or password',
+      });
+    }
+
+    const token = signToken(user._id);
+
+    res.status(200).json({
+      status: 'success',
+      token,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
+exports.upload = async (req, res) => {
+  try {
+    const newAssignment = await Assignment.create({
+      userId: req.user.id,
+      task: req.body.task,
+      admin: req.body.admin,
+    });
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        assignment: newAssignment,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
+exports.getAdmins = async (req, res) => {
+  try {
+    const admins = await User.find({ role: 'admin' }).select('username email');
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        admins,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
